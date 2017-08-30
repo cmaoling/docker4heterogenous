782f971257e0e0dc7b9fca5eea32df7ddb7eb500
Colinas Maoling <colinas.maoling@t-online.de>
2017-08-30 18:59:29 +0200

    common prune script

--- docker-prune.sh
+++ docker-prune.sh
@@ -1,3 +1,14 @@
 #bin/bash
-docker container prune -f
-docker image prune -f
+if [ "$( { docker container prune --help | grep "is not a docker command" > outfile; } 2>&1 )" == "" ];
+then
+	echo "docker container prune (NEW)"
+	docker container prune -f
+	echo "docker images prune (NEW)"
+	#docker image prune -f
+else
+	echo "docker container prune (OLD)"
+	#docker ps --filter "status=exited" | grep -e '[yh]s ago' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+	echo "docker images prune (OLD>"
+	#docker rmi -f $(docker images | grep "<none>" | grep -e "[yh]s ago"  | awk "{print \$3}")
+fi
+
