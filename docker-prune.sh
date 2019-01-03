e63a11ad31c060617900e9da50e633d5fe727b4a
Colinas Maoling <cmaoling@t-o.de>
2019-01-03 21:00:52 +0100

    also look for created containers to remove

--- docker-prune.sh
+++ docker-prune.sh
@@ -8,6 +8,7 @@ then
 else
 	echo "docker container prune (OLD)"
 	docker ps --filter "status=exited" | grep -e '[yh]s ago' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+        docker ps --filter "status=created" | grep -e '[yh]s ago' | awk '{print $1}' | xargs --no-run-if-empty docker rm
 	echo "docker images prune (OLD>"
 	docker rmi -f $(docker images | grep "<none>" | grep -e "[yh]s ago"  | awk "{print \$3}")
 fi
